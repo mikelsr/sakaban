aca38f171d42f3a9cdd149511beeb435c95cd169
mikelsr <mikelsr@pm.me>
2019-03-21 20:36:30 +0100

    Expose Sakaban to the cleansing fire

--- fs/merkletree/node.go
+++
@@ -1,9 +0,0 @@
-package merkletree
-
-// Node represents a Node of a MerkleTree, be it a normal node or a leaf node
-type Node interface {
-	Hash() []byte
-	IsDir() bool
-	Name() string
-	Subnodes() []Node
-}
